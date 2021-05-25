41da8271e0351c77db130e892f4e38c2df7eefa5
gaston <litoralcreatives@gmail.com>
2021-05-25 17:03:33 -0300

    Make an awesome sticky browsable project page

--- js/home_js.js
+++ js/home_js.js
@@ -2,7 +2,7 @@ const ScrollFuncs = (e) => {
   let scroll = window.pageYOffset;
 
   const background = document.querySelector(".page-container");
-  background.style.top = -(scroll * 0.05) + "px";
+  background.style.backgroundPositionY = -(scroll * 0.1) + "px";
 
   const nbb = document.getElementById("navbar-brand");
   if (scroll > 10) {
@@ -16,6 +16,31 @@ window.addEventListener("scroll", function (e) {
   ScrollFuncs(e);
 });
 
-// document.querySelector("#first-project").scrollIntoView({
-//   behavior: "smooth",
-// });
+const ScrollToElement = (element) => {
+  element.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+
+const ToogleProject = (index) => {
+  let element;
+
+  switch (index) {
+    case 1:
+      element = document.getElementById("first-project");
+      break;
+    case 2:
+      element = document.getElementById("second-project");
+      break;
+    case 3:
+      element = document.getElementById("third-project");
+      break;
+    case 4:
+      element = document.getElementById("fourth-project");
+      break;
+  }
+
+  if (element) {
+    ScrollToElement(element);
+  }
+};
