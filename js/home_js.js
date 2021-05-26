3bb35c7eca48417962d7d56f733d89646a14097b
gaston <litoralcreatives@gmail.com>
2021-05-26 17:08:05 -0300

    Add specific content to each project and spotify gadget

--- js/home_js.js
+++ js/home_js.js
@@ -2,7 +2,7 @@ const ScrollFuncs = (e) => {
   let scroll = window.pageYOffset;
 
   const background = document.querySelector(".page-container");
-  background.style.backgroundPositionY = -(scroll * 0.1) + "px";
+  background.style.backgroundPositionY = -(scroll * 0.04) + "px";
 
   const nbb = document.getElementById("navbar-brand");
   if (scroll > 10) {
@@ -42,5 +42,92 @@ const ToogleProject = (index) => {
 
   if (element) {
     ScrollToElement(element);
+  }
+};
+
+const AddProjects = () => {
+  let first_project = document
+    .getElementById("first-project")
+    .getElementsByClassName("project-info")[0];
+
+  let third_project = document
+    .getElementById("third-project")
+    .getElementsByClassName("project-info")[0];
+
+  let fourth_project = document
+    .getElementById("fourth-project")
+    .getElementsByClassName("project-info")[0];
+
+  first_project.innerHTML = "";
+  third_project.innerHTML = "";
+  fourth_project.innerHTML = "";
+
+  const cachitas = projects.filter(
+    (element) => element.artist == "Cachitas Now"
+  );
+  const elkaiser = projects.filter((element) => element.artist == "El Kaiser");
+  const chichica = projects.filter((element) => element.artist == "Chichica");
+
+  for (let i = 0; i < cachitas.length; i++) {
+    const work = cachitas[i];
+    const work_element = `<div class="work">
+      <img
+        class="work-art"
+        src="${work.artwork}"
+        alt=""
+      />
+      <div class="work-details">
+        <iframe src="${work.spotify}" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <h3 class="work-title">Title: <strong>${work.name}</strong></h3>
+        <p class="work-description"><strong>Descripción: </strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nam non cumque animi assumenda voluptate facilis maiores, qui, recusandae, reiciendis illum voluptatibus architecto. Facilis unde facere quas voluptas delectus incidunt.</strong></p>
+      
+        </div>
+    </div>`;
+    first_project.insertAdjacentHTML("beforeend", work_element);
+  }
+
+  for (let i = 0; i < chichica.length; i++) {
+    const work = chichica[i];
+    const work_element = `<div class="work">
+      <img
+        class="work-art"
+        src="${work.artwork}"
+        alt=""
+      />
+      <div class="work-details">
+        <iframe src="${work.spotify}" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <h3 class="work-title">Title: <strong>${work.name}</strong></h3>
+        <p class="work-description"><strong>Descripción: </strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nam non cumque animi assumenda voluptate facilis maiores, qui, recusandae, reiciendis illum voluptatibus architecto. Facilis unde facere quas voluptas delectus incidunt.</strong></p>
+      </div>
+    </div>`;
+    third_project.insertAdjacentHTML("beforeend", work_element);
+  }
+
+  for (let i = 0; i < elkaiser.length; i++) {
+    const work = elkaiser[i];
+    const work_element = `<div class="work">
+      <img
+        class="work-art"
+        src="${work.artwork}"
+        alt=""
+      />
+      <div class="work-details">
+        ${
+          work.spotify
+            ? `<iframe
+              src="${work.spotify}"
+              width="100%"
+              height="80"
+              frameborder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+            ></iframe>`
+            : ""
+        }
+        <h3 class="work-title">Title: <strong>${work.name}</strong></h3>
+        <p class="work-description"><strong>Descripción: </strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nam non cumque animi assumenda voluptate facilis maiores, qui, recusandae, reiciendis illum voluptatibus architecto. Facilis unde facere quas voluptas delectus incidunt.</strong></p>
+      </div>
+    </div>`;
+    fourth_project.insertAdjacentHTML("beforeend", work_element);
   }
 };
---
+++ json/projects.js
@@ -0,0 +1,673 @@
+const projects = [
+  {
+    name: "Trampas",
+    artist: "Cachitas Now",
+    artwork: "media/portadas/cachitas_1.jpeg",
+    spotify: "https://open.spotify.com/embed/album/3eCen3pEYgGRBvYVncZqGS",
+    rol: ["Mezcla", "Post-producción"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Smooth and calm background rithms",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "Raough and raspy voice",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "Flat",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Yo Voy a Decidir",
+    artist: "Cachitas Now",
+    artwork: "media/portadas/cachitas_2.jpeg",
+    spotify: "https://open.spotify.com/embed/album/4uXlfhBOGnEJrdSThJ3dGr",
+    rol: ["Mezcla", "Post-producción"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "La Vuelta Del Gatito",
+    artist: "Cachitas Now",
+    artwork: "media/portadas/cachitas_3.jpeg",
+    spotify: "https://open.spotify.com/embed/album/6c5nI2zljpbCeNjNlItpf9",
+    rol: ["Mezcla", "Post-producción"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Te Voy a Contar Amiga",
+    artist: "Cachitas Now",
+    artwork: "media/portadas/cachitas_4.jpeg",
+    spotify: "https://open.spotify.com/embed/album/6M5IbkI4KPdKHXAuxhtTIb",
+    rol: ["Mezcla", "Post-producción"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Corazón",
+    artist: "Cachitas Now",
+    artwork: "media/portadas/cachitas_5.jpeg",
+    spotify: "https://open.spotify.com/embed/album/7xV5H9GdycFFvKBxyUgWfN",
+    rol: ["Mezcla", "Post-producción"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Que Cheta",
+    artist: "Cachitas Now",
+    artwork: "media/portadas/cachitas_6.jpeg",
+    spotify: "https://open.spotify.com/embed/album/3XOZVeWoSoiVOtoi8uyJUs",
+    rol: ["Mezcla", "Post-producción"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Te Re Cabió",
+    artist: "Cachitas Now",
+    artwork: "media/portadas/cachitas_7.jpeg",
+    spotify: "https://open.spotify.com/embed/album/3CgZQ8JZKMp81MQOz6QBfO",
+    rol: ["Mezcla", "Post-producción"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Chonga",
+    artist: "Cachitas Now",
+    artwork: "media/portadas/cachitas_8.jpeg",
+    spotify: "https://open.spotify.com/embed/album/4Wd51SXwRmt1RRFKU7OLKo",
+    rol: ["Mezcla", "Post-producción"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Alma de Botón",
+    artist: "Agua Sucia y los Mareados",
+    rol: ["Grabación", "Edición", "Mezcla", "Mástering"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "En Los Techos Carnabal",
+    artist: "Agua Sucia y los Mareados",
+    rol: ["Grabación", "Edición", "Mezcla", "Mástering"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "La Cumparsita",
+    artist: "Agua Sucia y los Mareados",
+    rol: ["Grabación", "Edición", "Mezcla", "Mástering"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Los Invisibles",
+    artist: "Agua Sucia y los Mareados",
+    rol: ["Grabación", "Edición", "Mezcla", "Mástering"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Vas a Ver Cuando Estés Solo",
+    artist: "Agua Sucia y los Mareados",
+    rol: "Grabación - Edición - Mezcla - Mástering",
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Si Te Dan Ganas",
+    artist: "Agua Sucia y los Mareados",
+    rol: ["Grabación", "Edición", "Mezcla", "Mástering"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Hoy Voy a Bailar",
+    artist: "Agua Sucia y los Mareados",
+    rol: ["Grabación", "Edición", "Mezcla", "Mástering"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Hoy Nos Vamos de Joda",
+    artist: "Agua Sucia y los Mareados",
+    rol: ["Grabación", "Edición", "Mezcla", "Mástering"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Mano a Mano",
+    artist: "Agua Sucia y los Mareados",
+    rol: ["Grabación", "Edición", "Mezcla", "Mástering"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Como Estás",
+    artist: "El Kaiser",
+    artwork: "media/portadas/elkaiser_1.jpg",
+    rol: ["Grabación", "Edición", "Mezcla"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Siéntelo",
+    artist: "El Kaiser",
+    artwork: "media/portadas/elkaiser_2.jpeg",
+    spotify: "https://open.spotify.com/embed/album/5ke5eHmcunODcyaBru6HGE",
+    rol: ["Edición", "Mezcla"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Sonreír",
+    artist: "El Kaiser",
+    artwork: "media/portadas/elkaiser_3.jpg",
+    spotify: "https://open.spotify.com/embed/album/3iEWlRMw8x6HJgDVJqXveC",
+    rol: ["Grabación", "Edición", "Mezcla"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Corazón de Mi País",
+    artist: "El Kaiser",
+    artwork: "media/portadas/elkaiser_4.jpg",
+    spotify: "https://open.spotify.com/embed/album/0X4ivA73kf0PLgMLJHxvsF",
+    rol: ["Grabación", "Edición", "Mezcla"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Mejor Me Alejo",
+    artist: "El Kaiser",
+    artwork: "media/portadas/elkaiser_5.jpeg",
+    spotify: "https://open.spotify.com/embed/album/1FjSlLPvRO1Iq6rVK6TpXZ",
+    rol: ["Grabación", "Edición", "Mezcla"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "La Curiosidad / Caramelo / Relación (Live)",
+    artist: "Chichica",
+    artwork: "media/portadas/chichica_1.png",
+    spotify: "https://open.spotify.com/embed/album/0Boeqitz2lmRkCzscAO9mF",
+    rol: ["Producción", "Mezcla"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+  {
+    name: "Rápido y Seguro",
+    artist: "Lucio Consolo",
+    artwork: "media/portadas/lucioconsolo_1.jpeg",
+    rol: ["Pre-producción"],
+    instruments: [
+      {
+        name: "Guitar",
+        description: "Rithm funcky guitar",
+        plugins: ["Delay", "Reverb"],
+      },
+      {
+        name: "Voice",
+        description: "",
+        plugins: ["Reverb"],
+      },
+      {
+        name: "Piano",
+        description: "High notes",
+        plugins: ["none"],
+      },
+      {
+        name: "Drums",
+        description: "Low Snore",
+        plugins: ["none"],
+      },
+    ],
+  },
+];
